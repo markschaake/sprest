04b65cc1de5d216c9cd0baed1c3fe59b1102b294
Mark Schaake <mark.schaake@gmail.com>
2013-03-14 22:43:49 -0700

    Added json protocol to ModelCompanion

--- sprest-reactivemongo/src/main/scala/sprest/reactivemongo/ModelCompanion.scala
+++ sprest-reactivemongo/src/main/scala/sprest/reactivemongo/ModelCompanion.scala
@@ -3,7 +3,7 @@ package sprest.reactivemongo
 import spray.json._
 import sprest.models.Model
 
-trait ModelCompanion[M <: Model[ID], ID] {
+trait ModelCompanion[M <: Model[ID], ID] extends DefaultJsonProtocol {
   import reactivemongo.bson.handlers.{ BSONReader, BSONWriter }
   import reactivemongo.bson._
 
