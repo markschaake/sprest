e468cdd20e9d641c5bbd5ac18e4b80fcc0deb0ca
Mark Schaake <mark.schaake@gmail.com>
2013-03-18 15:41:11 -0700

    Removed unnecessary DateTime format from example

--- examples/sprest-reactivemongo-example/src/main/scala/models/Reminder.scala
+++ examples/sprest-reactivemongo-example/src/main/scala/models/Reminder.scala
@@ -12,17 +12,7 @@ case class Reminder(
   var id: Option[String] = None) extends Model[String]
 
 object Reminder extends ModelCompanion[Reminder, String] {
-
-  implicit object DateTimeFormat extends JsonFormat[DateTime] {
-    def read(json: JsValue) = json match {
-      case JsNumber(value) if value.isValidLong => new DateTime(value.toLong)
-      case JsString(strDate)                    => new DateTime(strDate)
-      case _                                    => throw new Exception("invalid format")
-    }
-    def write(date: DateTime) = JsNumber(date.getMillis)
-  }
-
+  import sprest.Formats._
   implicit val reminderJsonFormat = jsonFormat4(Reminder.apply _)
-
 }
 
