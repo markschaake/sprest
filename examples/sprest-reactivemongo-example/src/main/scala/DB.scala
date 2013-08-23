1e7756cd2b2d817907b441102775c7738fb351ef
Ivan Sharamet <isharamet@gmail.com>
2013-08-23 18:59:44 +0300

    Fix compilation errors caused by implicit session parameter

--- examples/sprest-reactivemongo-example/src/main/scala/DB.scala
+++ examples/sprest-reactivemongo-example/src/main/scala/DB.scala
@@ -11,7 +11,6 @@ object DB extends ReactiveMongoPersistence {
   import sprest.models.UUIDStringId
   import sprest.models.UniqueSelector
   import models._
-  import sprest.examples.reactivemongo.security.Session
   import scala.concurrent.ExecutionContext
 
   val driver = new MongoDriver
@@ -22,14 +21,14 @@ object DB extends ReactiveMongoPersistence {
   // mapped to "id" in JSON in all cases
   implicit object JsonTypeMapper extends SprayJsonTypeMapper with NormalizedIdTransformer
 
-  abstract class UnsecuredDAO[M <: sprest.models.Model[String]](collName: String)(implicit jsformat: RootJsonFormat[M]) extends CollectionDAO[M, String, Session](db(collName)) {
+  abstract class UnsecuredDAO[M <: sprest.models.Model[String]](collName: String)(implicit jsformat: RootJsonFormat[M]) extends CollectionDAO[M, String](db(collName)) {
 
-    case class Selector(id: String, session: Option[Session]) extends UniqueSelector[M, String, Session]
+    case class Selector(id: String) extends UniqueSelector[M, String]
 
-    override implicit def generateSelector(id: String, maybeSession: Option[Session]) = Selector(id, maybeSession)
-    override protected def addImpl(m: M)(implicit maybeSession: Option[Session], ec: ExecutionContext) = doAdd(m)
-    override protected def updateImpl(m: M)(implicit maybeSession: Option[Session], ec: ExecutionContext) = doUpdate(m)
-    override def remove(selector: Selector)(implicit maybeSession: Option[Session], ec: ExecutionContext) = uncheckedRemoveById(selector.id)
+    override implicit def generateSelector(id: String) = Selector(id)
+    override protected def addImpl(m: M)(implicit ec: ExecutionContext) = doAdd(m)
+    override protected def updateImpl(m: M)(implicit ec: ExecutionContext) = doUpdate(m)
+    override def remove(selector: Selector)(implicit ec: ExecutionContext) = uncheckedRemoveById(selector.id)
   }
 
   // MongoDB collections:
