109f222795fc283bfdc804e05cc003ff0a39c88f
Mark Schaake <mark.schaake@gmail.com>
2013-10-30 10:20:16 -0700

    Upgrade to spray 1.2-RC2, joda time 2.3, joda convert 1.5, specs2 2.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,10 +15,10 @@ object Dependencies {
   def runtime(deps: ModuleID*): Seq[ModuleID] = deps map (_ % "runtime")
   def container(deps: ModuleID*): Seq[ModuleID] = deps map (_ % "container")
 
-  val sprayVersion = "1.2-RC1"
+  val sprayVersion = "1.2-RC2"
   def sprayModule(id: String) = "io.spray" % id % sprayVersion
 
-  val akkaVersion = "2.2.1"
+  val akkaVersion = "2.2.3"
   def akkaModule(id: String) = "com.typesafe.akka" %% id % akkaVersion
 
   val sprayCan = sprayModule("spray-can")
@@ -26,11 +26,11 @@ object Dependencies {
   val sprayTestKit = sprayModule("spray-testkit")
   val sprayJson = "io.spray" %% "spray-json" % "1.2.5"
   val logback = "ch.qos.logback" % "logback-classic" % "1.0.1"
-  val joda = "joda-time" % "joda-time" % "2.1"
-  val jodaConvert = "org.joda" % "joda-convert" % "1.3"
-  val specs2 = "org.specs2" %% "specs2" % "2.1"
+  val joda = "joda-time" % "joda-time" % "2.3"
+  val jodaConvert = "org.joda" % "joda-convert" % "1.5"
+  val specs2 = "org.specs2" %% "specs2" % "2.2"
   val akkaActor = akkaModule("akka-actor")
   val akkaTestKit = akkaModule("akka-testkit")
-  val slick = "com.typesafe.slick" %% "slick" % "1.0.0"
+  val slick = "com.typesafe.slick" %% "slick" % "1.0.1"
   val reactiveMongo = "org.reactivemongo" %% "reactivemongo" % "0.10.0-SNAPSHOT"
 }
